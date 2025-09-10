4e97f94529d0d54033512151b784b1ef7e822470
Samuel Malec <malec293@gmail.com>
2025-09-10 23:17:30 +0200

    feat: create symbol resolver

--- cool/compiler/analysis/semantic_analyzer.hpp
+++ cool/compiler/analysis/semantic_analyzer.hpp
@@ -7,12 +7,14 @@
 namespace cool::compiler::ast {
 struct Expr;
 struct Stmt;
-}
+} // namespace cool::compiler::ast
 namespace cool::compiler::analysis {
-struct Resolver {
+struct Resolver
+{
     void resolve(const std::vector<std::unique_ptr<ast::Stmt>> &statements);
 
     std::pmr::unordered_map<std::string, std::string> symbol_table;
     void resolve_stmt(const std::unique_ptr<ast::Stmt> &stmt);
     void resolve_expr(const std::unique_ptr<ast::Expr> &expr);
-}
+};
+} // namespace cool::compiler::analysis
